552f7b71409a41c70d59a6ad4f2b29bdaa1c9c8a
FabianTorricoORT <Fabian.-ignaciooutlook.com>
2025-11-11 19:45:30 -0300

    Agrego función promedio y test Torrico

--- tests/test_es_par.py
+++
@@ -1,6 +0,0 @@
-# tests/test_es_par.py
-from funciones.es_par import es_par
-
-def test_es_par():
-    assert es_par(4) is True
-    assert es_par(7) is False
---
+++ tests/test_promedio.py
@@ -0,0 +1,5 @@
+from funciones.promedio import promedio
+
+def test_promedio():
+    assert promedio([2, 4, 6]) == 4
+    assert promedio([]) is None
