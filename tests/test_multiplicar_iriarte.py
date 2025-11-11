21813dde26a6c7116d9459e644ff2f27fd2758de
ManezDev <migueliriarte.ar@gmail.com>
2025-11-11 20:07:54 -0300

    Corrijo import

--- tests/test_multiplicar_iriarte.py
+++ tests/test_multiplicar_iriarte.py
@@ -1,6 +1,5 @@
-from funciones.multiplicar_iriarte import multiplicar_iriarte 
+from funciones.multiplicar_iriarte import multiplicar
 
 def test_multiplicar():
     assert multiplicar(3, 4) == 12
     assert multiplicar(-2, 5) == -10
-
