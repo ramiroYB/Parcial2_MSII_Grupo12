938243c7dd5e8d9d54fc585a7c60122463140777
Melgromo <melinaescudero.me@gmail.com>
2025-11-11 20:06:11 -0300

    Agrego funcion promedio_escudero y su test

--- tests/test_promedioescudero.py
+++ tests/test_promedioescudero.py
@@ -1,6 +1,3 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from funciones.promedioescudero import promedio
 def test_promedio():
     assert promedio([2, 4, 6]) == 4
