2b926beaa96804e929f5c19a0097d1775171cd1d
Jose Grabiel <jose.gabriel@dacinci.edu.ar>
2025-11-11 19:23:26 -0300

    Agrego funcion dividir_jose_Grabiel y su test exitoso

--- tests/test_dividir_jose_grabiel.py
+++ tests/test_dividir_jose_grabiel.py
@@ -1,5 +1,5 @@
-from funciones.dividir import dividir
+from funciones.dividir_jose_Grabiel import dividir_jose_Grabiel
 
 def test_dividir():
-    assert dividir(10, 2) == 5
-    assert dividir(5, 0) is None
+    assert dividir_jose_Grabiel(10, 2) == 5
+    assert dividir_jose_Grabiel(5, 0) is None
