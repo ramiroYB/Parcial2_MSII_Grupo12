0e068245eea9370f5054f62a0345575acfea1c66
Melgromo <melinaescudero.me@gmail.com>
2025-11-11 20:08:33 -0300

    Agrego funcion promedio_escudero y su test

--- funciones/promedioescudero.py
+++ funciones/promedioescudero.py
@@ -1,5 +1,5 @@
 def promedio(numeros):
- """Devuelve el promedio de una lista de números."""
+ """Devuelve el promedio de una lista de números"""
     if not numeros:
         return None
     return sum(numeros) / len(numeros)
