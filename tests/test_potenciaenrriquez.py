8545775494b04ef78e58053d733cdd60c40b7a9c
Ezequiel <ezequiel.e2123@gmail.com>
2025-11-11 19:12:52 -0300

    Agrego funcion de potencia enrriquez 2

--- tests/test_potenciaenrriquez.py
+++ tests/test_potenciaenrriquez.py
@@ -1,5 +1,5 @@
 #tests/test_potencia.py
-from funciones.potencia import potencia 
+from funciones.potenciaenrriquez import potencia 
 def test_potencia(): 
     assert potencia(2, 3) == 8 
-    assert potencia(5, 0) == 1 
+    assert potencia(5, 0) == 1
