3b5395cb02c6c052ed039a5d034d0c6efd99f37b
Melgromo <melinaescudero.me@gmail.com>
2025-11-11 20:38:15 -0300

    Agrego funcion raiz_cuadrada y su test

--- funciones/promedioescudero.py
+++
@@ -1,5 +0,0 @@
-def promedio(numeros):
- """Devuelve el promedio de una lista de números."""
-    if not numeros:
-        return None
-    return sum(numeros) / len(numeros)
--- tests/test_promedioescudero.py
+++
@@ -1,5 +0,0 @@
-from funciones.promedioescudero import promedio
-
-def test_promedio():
-    assert promedio([2, 4, 6]) == 4
-    assert promedio([]) is None
